d3c484c3cd5d26a4277b4611c158a1b9cacad289
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-08 06:17:23 +0300

    practice number methods

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+var num1 = 2
+var num2 = 31
+var multiply = num1 * num2
+
+var random = Math.ceil(Math.random())
+Number.isInteger(random)
+
+var num3 = 25
+var num4 = 7
+var mod = num3 % num4
+
+var max = Math.max(1,2,3,4,7,8,20)
+console.log(max)
+
